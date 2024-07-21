33aeabda884389560db39cbe0144e48755b36b7a
Chong Xian <chongxian@easystore.co>
2024-07-21 23:06:45 +0800

    update grammar

--- app/page.js
+++ app/page.js
@@ -69,7 +69,7 @@ export default function Home() {
   return (
     <div onMouseMove={onMouseMove} className="h-full">
       <main className="flex flex-col justify-between items-center py-5 sm:py-10">
-        <div className="text-xl text-center font-medium">{isRevealed ? "It is a" : "Chong Xian & Tan Ye baby's gender revealed in"}</div>
+        <div className="text-xl text-center font-medium">{isRevealed ? "It is a" : "Chong Xian & Tan Ye baby's gender will be revealed in"}</div>
         {MemoCountDownSection}
         <a href="https://www.chongxian.dev" target="_blank" className="cursor-pointer">
           Built by Chong Xian
