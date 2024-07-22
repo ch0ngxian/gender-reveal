3dbae719c26fa6d5f645a3abc161d7505296423e
Chong Xian <chongxian@easystore.co>
2024-07-22 09:48:37 +0800

    handle built by z index

--- app/page.js
+++ app/page.js
@@ -71,7 +71,7 @@ export default function Home() {
       <main className="flex flex-col justify-between items-center py-5 sm:py-10">
         <div className="text-xl text-center font-medium">{isRevealed ? "It is a" : "Chong Xian & Tan Ye baby's gender will be revealed in"}</div>
         {MemoCountDownSection}
-        <a href="https://www.chongxian.dev" target="_blank" className="cursor-pointer">
+        <a href="https://www.chongxian.dev" target="_blank" className="cursor-pointer" style={{ zIndex: 100000 }}>
           Built by Chong Xian
         </a>
       </main>
